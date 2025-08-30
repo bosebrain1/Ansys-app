21c8075e6cc9eb55fba5c75339c52d25d878cbe9
bosebrain1 <bose.brain1@gmail.com>
2025-08-29 20:55:54 -0400

    feat: add variables for both private and public subnets

--- nginx-module/network/variables.tf
+++ nginx-module/network/variables.tf
@@ -9,3 +9,33 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "public_subnet_a_cidr" {
+  description = "CIDR for public subnet A"
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_b_cidr" {
+  description = "CIDR for public subnet B"
+  default     = "10.0.2.0/24"
+}
+
+variable "public_subnet_c_cidr" {
+  description = "CIDR for public subnet C"
+  default     = "10.0.3.0/24"
+}
+
+variable "private_subnet_a_cidr" {
+  description = "CIDR for private subnet A"
+  default     = "10.0.11.0/24"
+}
+
+variable "private_subnet_b_cidr" {
+  description = "CIDR for private subnet B"
+  default     = "10.0.12.0/24"
+}
+
+variable "private_subnet_c_cidr" {
+  description = "CIDR for private subnet C"
+  default     = "10.0.13.0/24"
+}
