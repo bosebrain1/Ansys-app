2fd7d1abad129c583d2d6a2576967babc1dc34df
bosebrain1 <bose.brain1@gmail.com>
2025-08-30 00:10:08 -0400

    feat:provide variables for main.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,17 @@
+variable "region" {}
+variable "vpc_cidr" {}
 
+variable "public_subnet_a_cidr" {}
+variable "public_subnet_b_cidr" {}
+variable "public_subnet_c_cidr" {}
+
+variable "private_subnet_a_cidr" {}
+variable "private_subnet_b_cidr" {}
+variable "private_subnet_c_cidr" {}
+
+variable "az_a" {}
+variable "az_b" {}
+variable "az_c" {}
+
+variable "ami_id" {}
+variable "instance_type" {}
