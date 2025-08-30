18cbcfaf0739f59eb6fcc12bef5c7268345027d4
bosebrain1 <bose.brain1@gmail.com>
2025-08-29 20:49:53 -0400

    feat: add private subnets

--- nginx-module/network/main.tf
+++ nginx-module/network/main.tf
@@ -33,3 +33,24 @@ resource "aws_subnet" "public_subnet_c" {
   availability_zone       = var.az_c
   map_public_ip_on_launch = true
 }
+
+resource "aws_subnet" "private_subnet_a" {
+  vpc_id            = aws_vpc.nginx_vpc.id
+  cidr_block        = var.private_subnet_a_cidr
+  availability_zone = var.az_a
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "private_subnet_b" {
+  vpc_id            = aws_vpc.nginx_vpc.id
+  cidr_block        = var.private_subnet_b_cidr
+  availability_zone = var.az_b
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "private_subnet_c" {
+  vpc_id            = aws_vpc.nginx_vpc.id
+  cidr_block        = var.private_subnet_c_cidr
+  availability_zone = var.az_c
+  map_public_ip_on_launch = false
+}
