765b0b931f31e33acf8228770ed2383a19d7c4c0
bosebrain1 <bose.brain1@gmail.com>
2025-08-30 00:25:02 -0400

    feat: updated the output.tf file to contain vpc id and public subnets

--- nginx-module/network/output.tf
+++ nginx-module/network/output.tf
@@ -1,1 +1,9 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.nginx_vpc.id
+}
 
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_b.id, aws_subnet.public_subnet_c.id]
+}
