a28b5c222659ace0fa9b951963b9d9042b901de6
bosebrain1 <bose.brain1@gmail.com>
2025-08-29 20:46:56 -0400

    feat:  add new VPC module and aws igw

--- nginx-module/network/main.tf
+++ nginx-module/network/main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  region = var.region
+}
 
+resource "aws_vpc" "nginx_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+}
+
+resource "aws_internet_gateway" "nginx_igw" {
+  vpc_id = aws_vpc.nginx_vpc.id
+}
