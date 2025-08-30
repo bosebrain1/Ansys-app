506011dbc273b7fbab4bc357f5ea85c0fa21c578
bosebrain1 <bose.brain1@gmail.com>
2025-08-29 20:48:44 -0400

    feat: add public subnets

--- nginx-module/network/main.tf
+++ nginx-module/network/main.tf
@@ -12,3 +12,24 @@ resource "aws_vpc" "nginx_vpc" {
 resource "aws_internet_gateway" "nginx_igw" {
   vpc_id = aws_vpc.nginx_vpc.id
 }
+
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id                  = aws_vpc.nginx_vpc.id
+  cidr_block              = var.public_subnet_a_cidr
+  availability_zone       = var.az_b
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id                  = aws_vpc.nginx_vpc.id
+  cidr_block              = var.public_subnet_b_cidr
+  availability_zone       = var.az_b
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_subnet_c" {
+  vpc_id                  = aws_vpc.nginx_vpc.id
+  cidr_block              = var.public_subnet_c_cidr
+  availability_zone       = var.az_c
+  map_public_ip_on_launch = true
+}
