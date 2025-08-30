f8b5f56a0497cf3003e7201fe573a6960e8d7f0f
bosebrain1 <bose.brain1@gmail.com>
2025-08-29 21:04:29 -0400

    feat: add variables for ami and instance type

--- nginx-module/ec2-instance/variables.tf
+++ nginx-module/ec2-instance/variables.tf
@@ -1,1 +1,12 @@
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+  default     = "ami-00ca32bbc84273381" 
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
 
