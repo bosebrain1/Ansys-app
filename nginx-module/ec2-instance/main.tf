3815430710141552632fe83a98664aa7ad6356bb
bosebrain1 <bose.brain1@gmail.com>
2025-08-29 21:00:54 -0400

    feat: create security group and ec2 instance

--- nginx-module/ec2-instance/main.tf
+++ nginx-module/ec2-instance/main.tf
@@ -1,1 +1,46 @@
+resource "aws_security_group" "ec2_sec_grp" {
+  vpc_id = aws_vpc.nginx_vpc.id
+  name   = "ec2-sg"
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_instance" "nginx_server" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public_subnet_a.id
+  vpc_security_group_ids      = [aws_security_group.ec2_sec_grp.id]
+  associate_public_ip_address = true
+  
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras enable nginx1
+              yum install -y nginx
+              systemctl enable nginx
+              systemctl start nginx
+              echo "<h1>Hello from Terraform NGINX Server</h1>" > /usr/share/nginx/html/index.html
+              EOF
+
+}
