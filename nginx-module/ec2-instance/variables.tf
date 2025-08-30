3b8a4788337fb1fc0720967d584e7507f0884b2a
bosebrain1 <bose.brain1@gmail.com>
2025-08-30 00:36:04 -0400

    feat: updated the variables.tf file with vpc and subnet values

--- nginx-module/ec2-instance/variables.tf
+++ nginx-module/ec2-instance/variables.tf
@@ -1,3 +1,13 @@
+variable "vpc_id" {
+  description = "VPC ID to launch EC2 instance in"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where the EC2 instance will be placed"
+  type        = string
+}
+
 variable "ami_id" {
   description = "AMI ID for EC2 instance"
   type        = string
