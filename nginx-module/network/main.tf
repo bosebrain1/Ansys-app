eaf6abb9328c60fa38f506a1a35e5fba73368312
bosebrain1 <bose.brain1@gmail.com>
2025-08-29 20:52:41 -0400

    feat: add route table and route table association for public subnets

--- nginx-module/network/main.tf
+++ nginx-module/network/main.tf
@@ -54,3 +54,28 @@ resource "aws_subnet" "private_subnet_c" {
   availability_zone = var.az_c
   map_public_ip_on_launch = false
 }
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.nginx_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.nginx_igw.id
+  }
+
+}
+
+resource "aws_route_table_association" "public_assoc_a" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_assoc_b" {
+  subnet_id      = aws_subnet.public_subnet_b.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_assoc_c" {
+  subnet_id      = aws_subnet.public_subnet_c.id
+  route_table_id = aws_route_table.public_route_table.id
+}
